9f1435697823dae2552da3ba4534af554c2ee8b6
Daniel Jensen <45940133+Darkrenga@users.noreply.github.com>
2020-10-09 11:39:15 +0200

    Update js.js

--- JS/js-partials/js.js
+++ JS/js-partials/js.js
@@ -1,5 +1,7 @@
-let nav = document.getElementById("nav");
+let nav = document.getElementById("nav"); //Gets the id that i'll add it too, make sure they match
 
+
+//Creating my html as a variable
 let htmlNav = `
         <ul>
             <li>
@@ -12,4 +14,5 @@ let htmlNav = `
         </ul>
     `
 
-nav.insertAdjacentHTML("afterbegin", htmlNav);
+//I assign the dom i created in htmlNav to my variable nav which is connected to nav. This adds the html to the dom
+nav.innerHTML = htmlNav;
